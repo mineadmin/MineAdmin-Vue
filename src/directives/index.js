1868499c89ad8a623cf57fdc7815710326c74d52
X.Mo <root@imoi.cn>
2022-07-28 12:43:15 +0800

    增加copy指令

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,10 +1,12 @@
 import auth from './auth/index'
 import role from './role/index'
+import copy from './copy/index'
 
 
 export default {
   install (Vue) {
     Vue.directive('auth', auth)
     Vue.directive('role', role)
+    Vue.directive('copy', copy)
   }
 }
