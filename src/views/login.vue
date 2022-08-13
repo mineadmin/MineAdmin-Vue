6493d214e1dc1182ab752a44e35b94249fa2e9b7
SpiderMan <zx18602780217@gmail.com>
2022-08-13 09:40:04 +0800

    feat: 删除登录验证码

--- src/views/login.vue
+++ src/views/login.vue
@@ -8,46 +8,44 @@
  - @Link   https://gitee.com/xmo/mineadmin-vue
 -->
 <script setup>
-  import { reactive, ref, onMounted } from 'vue'
-  import VerifyCode from '@cps/ma-verifyCode/index.vue'
-  import { Message } from '@arco-design/web-vue'
-  import { useUserStore } from '@/store'
-  import { useRouter, useRoute } from 'vue-router'
+import { reactive, ref, onMounted } from 'vue'
+import { useUserStore } from '@/store'
+import { useRouter, useRoute } from 'vue-router'
 
-  const router = useRouter()
-  const route  = useRoute()
+const router = useRouter()
+const route = useRoute()
 
-  const Verify = ref(null)
+const loading = ref(false)
+const form = reactive({ username: 'superAdmin', password: 'admin123' })
 
-  const loading = ref(false)
-  const form = reactive({ username: 'superAdmin', password: 'admin123', code: '' })
+const userStore = useUserStore()
 
-  const userStore = useUserStore()
+const redirect = route.query.redirect ? route.query.redirect : '/'
 
-  const redirect = route.query.redirect ? route.query.redirect : '/'
-
-  const handleSubmit = async ({ values, errors }) => {
-    if (loading.value) {
+const handleSubmit = async ({ values, errors }) => {
+  if (loading.value) {
+    return
+  }
+  loading.value = true
+  if (!errors) {
+    const result = await userStore.login(form)
+    if (!result) {
+      loading.value = false
       return
     }
-    loading.value = true
-    if (Verify.value.checkResult(form.code) && (! errors)) {
-      const result = await userStore.login(form)
-      if (! result) {
-        loading.value = false
-        return
-      }
-      router.push(redirect)
-    }
-    loading.value = false
+    router.push(redirect)
   }
+  loading.value = false
+}
 </script>
 <template>
   <div class="login-container">
     <div class="login-width mx-auto flex justify-between h-full items-center">
       <div class="w-6/12 mx-auto left-panel rounded-l pl-5 pr-5 hidden md:block bg-blue-50">
-        <div class="logo"><img src="/logo.svg" width="45"><span>HGYKAdmin</span></div>
-        <div class="slogan flex justify-end"><span>---- {{ $t('sys.login.slogan') }}</span></div>
+        <div class="logo"><img src="/logo.svg" width="45" /><span>HGYKAdmin</span></div>
+        <div class="slogan flex justify-end">
+          <span>---- {{ $t('sys.login.slogan') }}</span>
+        </div>
       </div>
 
       <div class="md:w-6/12 w-11/12 md:rounded-r mx-auto pl-5 pr-5 pb-10 bg-white">
@@ -74,36 +72,9 @@
             :hide-label="true"
             :rules="[{ required: true, message: $t('sys.login.passwordNotice') }]"
           >
-            <a-input-password
-              v-model="form.password"
-              :placeholder="$t('sys.login.password')"
-              size="large"
-              allow-clear
-            >
+            <a-input-password v-model="form.password" :placeholder="$t('sys.login.password')" size="large" allow-clear>
               <template #prefix><icon-lock /></template>
             </a-input-password>
-          </a-form-item>
-
-          <a-form-item
-            field="code"
-            :hide-label="true"
-            :rules="[{
-              required: true,
-              match: /^[a-zA-Z0-9]{4}$/,
-              message: $t('sys.login.verifyCodeNotice')
-            }]"
-          >
-            <a-input
-              v-model="form.code"
-              :placeholder="$t('sys.login.verifyCode')"
-              size="large"
-              allow-clear
-            >
-              <template #prefix><icon-safe /></template>
-              <template #append>
-                <verify-code ref="Verify" />
-              </template>
-            </a-input>
           </a-form-item>
 
           <a-form-item :hide-label="true" class="mt-5">
@@ -138,34 +109,39 @@
   }
 
   .left-panel {
-    height: 491px;
+    height: 433px;
     background-image: url(@/assets/login_picture.svg);
     background-repeat: no-repeat;
     background-position: center 60px;
-    background-size:contain;
+    background-size: contain;
   }
 
   .logo {
-    display: flex; margin-top: 20px; color: #333;
+    display: flex;
+    margin-top: 20px;
+    color: #333;
     span {
-      font-size: 28px; margin-left: 15px;
+      font-size: 28px;
+      margin-left: 15px;
       color: rgb(var(--primary-6));
     }
   }
   .slogan {
-    font-size: 16px; line-height: 50px;
+    font-size: 16px;
+    line-height: 50px;
   }
 
   :deep(.arco-input-append) {
     padding: 0 !important;
   }
 
-  .other-login{
+  .other-login {
     cursor: pointer;
   }
 
-  .qq:hover, .alipay:hover {
-    background: #165DFF;
+  .qq:hover,
+  .alipay:hover {
+    background: #165dff;
   }
   .wechat:hover {
     background: #0f9c02;
