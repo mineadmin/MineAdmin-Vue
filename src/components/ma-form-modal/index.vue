a9d4f7a1d24c24e0993902bd48d580899325e3bc
X.Mo <root@imoi.cn>
2023-01-05 15:16:58 +0800

    update

--- src/components/ma-form-modal/index.vue
+++ src/components/ma-form-modal/index.vue
@@ -1,14 +1,20 @@
 <template>
-  <a-modal v-model:visible="modal.visible"
-           :on-before-ok="modal.submit"
-           unmount-on-close
-           @cancel="modal.cancel"
+  <a-modal
+    v-model:visible="modal.visible"
+    :on-before-ok="modal.submit"
+    unmount-on-close
+    @cancel="modal.cancel"
   >
     <template #title>
       {{ prop.title }}
     </template>
     <slot name="body"></slot>
-    <ma-form ref="maFormRef" :columns="prop.column" v-model="form" :options="{...options, showButtons: false}"/>
+    <ma-form
+      ref="maFormRef"
+      :columns="prop.column"
+      v-model="form"
+      :options="{ ...options, showButtons: false }"
+    />
   </a-modal>
 </template>
 
@@ -19,57 +25,55 @@
  *  @visible(show = true|false) 显示关闭事件
  */
 
-import {reactive, ref, watch} from "vue";
-const emit = defineEmits(['visible','validateError', 'open', 'cancel', 'close'])
-const form = ref({})
+import { reactive, ref, watch } from "vue";
+const emit = defineEmits(["visible", "validateError", "open", "cancel", "close"]);
+const form = ref({});
 const prop = defineProps({
-  title: { type: String, default: ''}, // 弹出框标题
-  column: { type: Array, default: []}, // ma-form字段
-  default_visible: {type: Boolean, default: false}, // 默认隐藏
-  options: {type: Object, default: {}}, // ma-form 属性
-  submit: {type: Function, default: () => {}}
-})
+  title: { type: String, default: "" }, // 弹出框标题
+  column: { type: Array, default: [] }, // ma-form字段
+  default_visible: { type: Boolean, default: false }, // 默认隐藏
+  options: { type: Object, default: {} }, // ma-form 属性
+  submit: { type: Function, default: () => {} },
+});
 
-const maFormRef = ref()
+const maFormRef = ref();
 
 const modal = reactive({
   visible: prop.default_visible,
-  open(data){
-    modal.visible = true
-    emit('open', data)
+  open(data) {
+    modal.visible = true;
+    emit("open", data);
   },
-  close()
-  {
-    modal.visible = false
+  close() {
+    modal.visible = false;
   },
-  async submit()
-  {
-    let validate = await maFormRef.value.formRef.validate()
+  async submit() {
+    let validate = await maFormRef.value.formRef.validate();
     if (validate) {
-      emit('validateError', validate)
+      emit("validateError", validate);
       return false;
     }
-    return prop.submit(form._rawValue)
+    return prop.submit(form._rawValue);
   },
-  cancel()
-  {
-    emit('cancel')
-  }
-})
+  cancel() {
+    emit("cancel");
+  },
+});
 
-
-watch(() => modal.visible, (vl) => {
-  emit('visible', modal.visible)
-}, {immediate: true})
+watch(
+  () => modal.visible,
+  (vl) => {
+    emit("visible", modal.visible);
+  },
+  { immediate: true }
+);
 
 defineExpose({
   open: modal.open,
   close: modal.close,
   formData: form,
   form: maFormRef,
-})
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/ma-info/index.vue
+++ src/components/ma-info/index.vue
@@ -1,23 +1,21 @@
 <template>
   <a-space direction="vertical" size="large" fill>
-    <a-descriptions :data="descriptions"
-                    :column="props.column"
-                    :title="props.title"
-                    :layout="props.layout"
-                    :bordered="props.bordered"
-                    table-layout="fixed"
-                    :size="props.size"
-                    :label-style="props.labelStyle"
-                    :value-style="props.valueStyle"
-
+    <a-descriptions
+      :data="descriptions"
+      :column="props.column"
+      :title="props.title"
+      :layout="props.layout"
+      :bordered="props.bordered"
+      table-layout="fixed"
+      :size="props.size"
+      :label-style="props.labelStyle"
+      :value-style="props.valueStyle"
     >
       <a-descriptions-item v-for="item in descriptions" :label="item.label">
         <template v-if="item.formType === 'upload'">
           <a-image-preview-group infinite v-if="isArray(item.value)">
             <a-space>
-              <a-image v-for="src in item.value"
-                       :src="tool.viewImage(src)"
-                       width="50" />
+              <a-image v-for="src in item.value" :src="tool.viewImage(src)" width="50" />
             </a-space>
           </a-image-preview-group>
           <a-image v-else :src="tool.viewImage(item.value)" width="50"></a-image>
@@ -26,7 +24,9 @@
           <slot :name="item.dataIndex" :row="item" :data="data"></slot>
         </template>
         <template v-else-if="item.formType === 'radio'">
-          <a-tag color="blue">{{ item.dict.data.filter(row => row.value == item.value)[0]?.label }}</a-tag>
+          <a-tag color="blue">{{
+            item.dict.data.filter((row) => row.value == item.value)[0]?.label
+          }}</a-tag>
         </template>
         <div v-else>
           {{ item.value }}
@@ -41,14 +41,14 @@
  * 描述列表 Descriptions https://arco.design/vue/component/descriptions
  * @author NekGod
  */
-import {getCurrentInstance, ref, watch} from "vue";
+import { getCurrentInstance, ref, watch } from "vue";
 import tool from "@/utils/tool";
-import {isArray} from "lodash";
+import { isArray } from "lodash";
 
-const app = getCurrentInstance().appContext.app
-const columns = ref([])
-const data = ref({})
-const descriptions = ref([])
+const app = getCurrentInstance().appContext.app;
+const columns = ref([]);
+const data = ref({});
+const descriptions = ref([]);
 const props = defineProps({
   columns: { type: Array },
   data: {},
@@ -56,57 +56,51 @@ const props = defineProps({
     default: 3,
   },
   title: {
-    default: ""
+    default: "",
   },
   bordered: {
-    default: true
+    default: true,
   },
   layout: {
-    default: "horizontal"
+    default: "horizontal",
   },
   labelStyle: {
-    default: {
-
-    }
+    default: {},
   },
   valueStyle: {
-    default: {
-
-    }
+    default: {},
   },
   size: {
-    default: "mini"
-  }
-})
+    default: "mini",
+  },
+});
 
 watch(
-    () => props.columns,
-    (vl) =>{
-      columns.value = vl
-    },
-    { deep : true, immediate: true }
-)
+  () => props.columns,
+  (vl) => {
+    columns.value = vl;
+  },
+  { deep: true, immediate: true }
+);
 
 watch(
-    () => props.data,
-    (vl) => {
-      data.value = props.data
-      descriptions.value = []
-      columns.value.forEach((item) => {
-        if (item.dataIndex === '__operation' || item.infoShow === false) {
-          return ;
-        }
-        descriptions.value.push({
-          ...item,
-          label: item.title,
-          value: data.value[item.dataIndex],
-        })
-      })
-    },
-      { deep : true, immediate: true }
-)
+  () => props.data,
+  (vl) => {
+    data.value = props.data;
+    descriptions.value = [];
+    columns.value.forEach((item) => {
+      if (item.dataIndex === "__operation" || item.infoShow === false) {
+        return;
+      }
+      descriptions.value.push({
+        ...item,
+        label: item.title,
+        value: data.value[item.dataIndex],
+      });
+    });
+  },
+  { deep: true, immediate: true }
+);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
