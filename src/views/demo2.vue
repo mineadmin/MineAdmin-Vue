c709d5c15f47d575d5a39dda152cb89169c8e371
X.Mo <root@imoi.cn>
2023-03-14 15:32:52 +0800

    up

--- src/views/demo2.vue
+++ src/views/demo2.vue
@@ -1,11 +1,25 @@
 <template>
-  <ma-form v-model="form" :options="options" :columns="columns" />
+  <div class="ma-content-block p-5"><ma-form v-model="form" :options="options" :columns="columns" /></div>
 </template>
 <script setup>
 import { ref } from 'vue'
 
 const form = ref({})
 const options = ref({})
-const columns = ref([])
+const columns = ref([
+  {
+    formType: 'tabs',
+    customClass: ['mb-2'],
+    tabs: [
+      {
+        title: '基础信息',
+        formList: [{ formType: 'divider', title: '基础信息'}]
+      },
+      {
+        title: '规格库存',
+      },
+    ]
+  }
+])
 
 </script>
