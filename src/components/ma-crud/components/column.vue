45ab8766ee7d6e2181187096b58bdb520a63b85e
NEKGod <1559096467@qq.com>
2023-03-22 13:16:40 +0800

    操作栏修复布局问题

--- src/components/ma-crud/components/column.vue
+++ src/components/ma-crud/components/column.vue
@@ -55,59 +55,62 @@
           v-else
       >
         <template #cell="{ record, column, rowIndex }">
+          <!-- 操作栏 -->
           <template v-if="row.dataIndex === '__operation'">
-            <a-space size="mini">
-              <slot name="operationBeforeExtend" v-bind="{ record, column, rowIndex }"></slot>
-              <slot name="operationCell" v-bind="{ record, column, rowIndex }">
-                <!-- <a-link
-                  v-if="
-                    options.see.show
-                    && ($common.auth(options.see.auth || [])
-                    || (options.see.role || []))
-                  "
-                  type="primary"
-                ><icon-eye /> {{ options.see.text || '查看' }}</a-link> -->
-                <a-link
-                    v-if="(isFunction(options.edit.show) ? options.edit.show(record):options.edit.show) && !props.isRecovery"
-                    v-auth="options.edit.auth || []"
-                    v-role="options.edit.role || []"
+            <a-scrollbar type="track" style="overflow: auto;">
+              <a-space size="mini">
+                <slot name="operationBeforeExtend" v-bind="{ record, column, rowIndex }"></slot>
+                <slot name="operationCell" v-bind="{ record, column, rowIndex }">
+                  <!-- <a-link
+                    v-if="
+                      options.see.show
+                      && ($common.auth(options.see.auth || [])
+                      || (options.see.role || []))
+                    "
                     type="primary"
-                    @click="editAction(record)"
-                >
-                  <icon-edit /> {{ options.edit.text || '编辑' }}
-                </a-link>
-
-                <a-popconfirm
-                    content="确定要恢复该数据吗?"
-                    position="bottom"
-                    @ok="recoveryAction(record)"
-                    v-if="(isFunction(options.recovery.show) ? options.recovery.show(record):options.recovery.show) && props.isRecovery"
-                    v-auth="options.recovery.auth || []"
-                    v-role="options.recovery.role || []"
-                >
-                  <a-link type="primary"><icon-undo /> {{ options.recovery.text || '恢复' }} </a-link>
-                </a-popconfirm>
-
-                <a-popconfirm
-                    content="确定要删除该数据吗?"
-                    position="bottom"
-                    @ok="deleteAction(record)"
-                    v-if="(isFunction(options.delete.show) ? options.delete.show(record):options.delete.show)"
-                >
+                  ><icon-eye /> {{ options.see.text || '查看' }}</a-link> -->
                   <a-link
+                      v-if="(isFunction(options.edit.show) ? options.edit.show(record):options.edit.show) && !props.isRecovery"
+                      v-auth="options.edit.auth || []"
+                      v-role="options.edit.role || []"
                       type="primary"
-                      v-auth="options.delete.auth || []"
-                      v-role="options.delete.role || []"
+                      @click="editAction(record)"
                   >
-                    <icon-delete />
-                    {{
-                      props.isRecovery ? options.delete.realText || '删除' : options.delete.text || '删除'
-                    }}
+                    <icon-edit /> {{ options.edit.text || '编辑' }}
                   </a-link>
-                </a-popconfirm>
-              </slot>
-              <slot name="operationAfterExtend" v-bind="{ record, column, rowIndex }"></slot>
-            </a-space>
+
+                  <a-popconfirm
+                      content="确定要恢复该数据吗?"
+                      position="bottom"
+                      @ok="recoveryAction(record)"
+                      v-if="(isFunction(options.recovery.show) ? options.recovery.show(record):options.recovery.show) && props.isRecovery"
+                      v-auth="options.recovery.auth || []"
+                      v-role="options.recovery.role || []"
+                  >
+                    <a-link type="primary"><icon-undo /> {{ options.recovery.text || '恢复' }} </a-link>
+                  </a-popconfirm>
+
+                  <a-popconfirm
+                      content="确定要删除该数据吗?"
+                      position="bottom"
+                      @ok="deleteAction(record)"
+                      v-if="(isFunction(options.delete.show) ? options.delete.show(record):options.delete.show)"
+                  >
+                    <a-link
+                        type="primary"
+                        v-auth="options.delete.auth || []"
+                        v-role="options.delete.role || []"
+                    >
+                      <icon-delete />
+                      {{
+                        props.isRecovery ? options.delete.realText || '删除' : options.delete.text || '删除'
+                      }}
+                    </a-link>
+                  </a-popconfirm>
+                </slot>
+                <slot name="operationAfterExtend" v-bind="{ record, column, rowIndex }"></slot>
+              </a-space>
+            </a-scrollbar>
           </template>
           <template v-else-if="row.customRender">
             <custom-render
