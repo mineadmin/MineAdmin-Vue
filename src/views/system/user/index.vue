eb37254c6a4f5e6d59b200ff62e2800f457a517e
X.Mo <root@imoi.cn>
2023-04-20 16:46:17 +0800

    refactor: 明确声明用户密码添加时可修改，编辑时禁止修改

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -225,7 +225,7 @@
     },
     { 
       title: '密码', dataIndex: 'password', hide: true, autocomplete: 'off',
-      addDefaultValue: '123456', editDefaultValue: '', editDisabled: true, type: 'password', 
+      addDefaultValue: '123456', editDefaultValue: '', addDisabled: false, editDisabled: true, type: 'password',
       addRules: [{ required: true, message: '密码必填' }],
     },
     { title: '昵称', dataIndex: 'nickname', width: 120 },
