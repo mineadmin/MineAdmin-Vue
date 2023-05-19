9de22c2e4ef75dc4bf2606c4ba8cbd8e4323de99
X.Mo <root@imoi.cn>
2023-05-19 21:52:10 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 -->
 <script setup>
   import { checkVersion } from 'version-rocket'
-  import { version,admin_name } from '../package.json'
+  import { version, admin_name } from '../package.json'
   import cn from '@arco-design/web-vue/es/locale/lang/zh-cn'
   import en from '@arco-design/web-vue/es/locale/lang/en-us'
   import { ref } from 'vue'
@@ -17,17 +17,16 @@
   const appStore = useAppStore()
   const lang = ref(appStore.language === 'zh_CN' ? cn : en)
 
-
-  checkVersion({
-      localPackageVersion: version,
-      originVersionFileUrl: `${location.origin}/version.json`,
-      // 更多配置选项请参考 API
-  },{
-      title:admin_name,
-      description:"有新版本更新",
-      buttonText:"立即更新",
-      // cancelButtonText:"稍后更新",
-  })
+  // checkVersion({
+  //     localPackageVersion: version,
+  //     originVersionFileUrl: `${location.origin}/version.json`,
+  //     // 更多配置选项请参考 API
+  // },{
+  //     title:admin_name,
+  //     description:"有新版本更新",
+  //     buttonText:"立即更新",
+  //     // cancelButtonText:"稍后更新",
+  // })
 </script>
 
 <template>
