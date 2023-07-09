f32a1797bf3ed29cae443fedcbd5b6c50d122dc1
Administrator <admin@example.com>
2023-07-09 12:19:57 +0800

    feat:增加wangEdit富文本组件

---
+++ src/components/ma-form/formItem/form-wang-editor.vue
@@ -0,0 +1,55 @@
+<!--
+ - MineAdmin is committed to providing solutions for quickly building web applications
+ - Please view the LICENSE file that was distributed with this source code,
+ - For the full copyright and license information.
+ - Thank you very much for using MineAdmin.
+ -
+ - @Author X.Mo<root@imoi.cn>
+ - @Link   https://gitee.com/xmo/mineadmin-vue
+-->
+<template>
+  <ma-form-item
+    v-if="(typeof props.component.display == 'undefined' || props.component.display === true)"
+    :component="props.component"
+    :custom-field="props.customField"
+  >
+    <slot :name="`form-${props.component.dataIndex}`" v-bind="props.component">
+      <ma-wang-editor
+        v-model="value"
+        style="width: 100%;"
+        :height="props.component.height"
+        :id="props.component.id"
+        :mode="props.component.mode"
+        @change="maEvent.handleChangeEvent(props.component, $event)"
+      >
+      </ma-wang-editor>
+    </slot>
+  </ma-form-item>
+</template>
+
+<script setup>
+import { ref, inject, onMounted, watch } from 'vue'
+import { get, set } from 'lodash'
+import MaWangEditor from '@/components/ma-wangEditor/index.vue'
+import MaFormItem from './form-item.vue'
+import { maEvent } from '../js/formItemMixin.js'
+const props = defineProps({
+  component: Object,
+  customField: { type: String, default: undefined }
+})
+
+const formModel = inject('formModel')
+const index = props.customField ?? props.component.dataIndex
+const value = ref(get(formModel.value, index))
+
+watch( () => get(formModel.value, index), vl => value.value = vl )
+watch( () => value.value, v => {
+  set(formModel.value, index, v)
+  index.indexOf('.') > -1 && delete formModel.value[index]
+} )
+
+maEvent.handleCommonEvent(props.component, 'onCreated')
+onMounted(() => {
+  maEvent.handleCommonEvent(props.component, 'onMounted')
+})
+</script>
