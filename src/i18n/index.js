4cd77637bd798cce3369f78c84e25cd1389e2a2b
spencer.xu <xuyang@youzi-dm.com>
2023-07-25 13:37:48 +0800

    获取翻译文件方法替换&递归获取多层级翻译文件内容

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,20 +6,34 @@ const setting = tool.local.get('setting')
 const getLanguage = () => {
   const loadFile = () => {
     if (setting.language === 'zh_CN') {
-      return import.meta.globEager('./zh_CN/*.js')
+      return import.meta.glob('./zh_CN/**/*.js', {eager:true})
     } else if (setting.language === 'en') {
-      return import.meta.globEager('./en/*.js')
+      return import.meta.glob('./en/**/*.js', {eager:true})
     }
   }
 
+  const generateLanguage = (fileNames, fileContent, generateLanguages = {}) => {
+    const fileName = fileNames.shift()
+    if (fileNames.length > 0) {
+      if (typeof generateLanguages[fileName] == 'undefined') {
+        generateLanguages[fileName] = {}
+      }
+      generateLanguages[fileName] = generateLanguage(fileNames, fileContent, generateLanguages[fileName])
+    }else{
+      generateLanguages[fileName] = fileContent
+    }
+    return generateLanguages
+  }
+
   const files = loadFile()
-
-  const messages = { [setting.language]: {} }
-
+  let messages = { [setting.language]: {} }
   for (let path in files) {
-    const name = path.match(/([A-Za-z0-9_]+)/g)[1]
+    const names = path.match(/([A-Za-z0-9_]+)/g)
+    //去除语言文件夹和文件后缀名
+    names.shift()
+    names.pop()
     if (files[path].default) {
-      messages[setting.language][name] = files[path].default
+      messages[setting.language] = generateLanguage(names, files[path].default, messages[setting.language])
     }
   }
   return messages
