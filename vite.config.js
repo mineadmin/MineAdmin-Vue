01bb6d34facdff63e044f0950e52738d7f66cfa3
X.Mo <root@imoi.cn>
2023-09-28 11:51:18 +0800

    fix: 修复打包后出现的一些bug

--- vite.config.js
+++ vite.config.js
@@ -19,15 +19,15 @@ export default ({ mode }) => {
 
     build: {
       chunkSizeWarningLimit: 1500,
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return id.toString().split("node_modules/")[1].split("/")[0].toString();
-            }
-          }
-        }
-      }
+      // rollupOptions: {
+      //   output: {
+      //     manualChunks(id) {
+      //       if (id.includes('node_modules')) {
+      //         return id.toString().split("node_modules/")[1].split("/")[0].toString();
+      //       }
+      //     }
+      //   }
+      // }
     },
 
     server: {
