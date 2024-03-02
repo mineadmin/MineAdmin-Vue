928271ce744957a3957b13974cf1175f86e90987
xing.gu <xing.gu@louku.com>
2024-03-03 04:51:55 +0800

    - 修改vs code编辑器配置，增加Prettier工具，使代码具有一致的格式，统一编码风格 - 增加ESlint代码分析工具，提高代码质量

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+export default exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'warn',
+    'vue/multi-word-component-names': 'off',
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+};
