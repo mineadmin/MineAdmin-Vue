ac7cbd9fe3204caac80355a1f10781d8bb6937e0
王小大 <m@wangxiaoda.com>
2024-04-13 17:25:24 +0800

    fix:验证多角色code时异常

--- src/directives/role/index.js
+++ src/directives/role/index.js
@@ -6,9 +6,12 @@ const checkRole = (el, binding) => {
   if (Array.isArray(value)) {
     if (value.length > 0) {
       let isHas = false
-      value.map(item => {
-        isHas = role(item)
-      })
+      for(let i = 0; i < value.length; i++) {
+        isHas = role(value[i]);
+        if(isHas) {
+          break;
+        }
+      }
 
       if (!isHas && el.parentNode) {
         el.parentNode.removeChild(el)
