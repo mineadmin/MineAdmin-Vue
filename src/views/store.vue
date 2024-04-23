6a613ad00fe2bdcd8c266a507f56b90fd8a50d69
X.Mo <root@imoi.cn>
2024-04-23 16:34:49 +0800

    feat: app store

--- src/views/store.vue
+++ src/views/store.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref } from 'vue'
 
+const isDev = ref(import.meta.env.DEV)
 </script>
 
 <template>
-  <div>
-    asdasd
+  <div class="ma-content-block p-4 h-full overflow-y-auto">
+    <div v-if="isDev">
+      <div class="">
+
+      </div>
+    </div>
+    <div v-else>
+      <a-result status="info" title="说明">
+        <template #subtitle>
+          应用市场只在开发环境开放使用。
+        </template>
+      </a-result>
+    </div>
   </div>
 </template>
 
